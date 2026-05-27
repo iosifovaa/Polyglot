068bde50230dff2c540e1ee34dd3d93ae6a865d1
Akonya <iosifovaaktilek@gmail.com>
2026-05-27 17:48:16 +0500

    Update homepage design

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const button = document.querySelector("button");
+const buttons = document.querySelectorAll(".language-buttons button");
+const startButton = document.querySelector(".main-btn");
 
-button.addEventListener("click", () => {
-  alert("Добро пожаловать в Polyglot!");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    buttons.forEach(btn => btn.classList.remove("active"));
+    button.classList.add("active");
+    alert(`Выбран язык сайта: ${button.textContent}`);
+  });
+});
+
+startButton.addEventListener("click", () => {
+  alert("Скоро здесь будет регистрация и персональная анкета!");
 });
