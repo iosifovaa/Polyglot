279fbd0f192f18780f4a682e23b49de02745f9ac
Akonya <iosifovaaktilek@gmail.com>
2026-06-04 04:03:07 +0500

    Clean up lessons section and navigation

--- script.js
+++ script.js
@@ -77,40 +77,7 @@ backStepBtn.addEventListener("click", () => {
 }); 
 
 renderStep();
-const lessonButtons = document.querySelectorAll(".lesson-card button");
 
-const modal = document.querySelector("#lessonModal");
-const modalTitle = document.querySelector("#modalTitle");
-const closeModal = document.querySelector(".close-modal");
-const modalExample = document.querySelector("#modalExample");
-
-const lessonData = {
-  "Приветствие": "Hello! How are you? — Привет! Как дела?",
-  "Путешествия": "Where is the hotel? — Где находится отель?",
-  "Сериалы": "What are you up to? — Чем занимаешься?"
-};
-
-lessonButtons.forEach(button => {
-  button.addEventListener("click", () => {
-    const lessonTitle =
-      button.parentElement.querySelector("h3").textContent;
-
-    modalTitle.textContent = lessonTitle;
-    modalExample.textContent = lessonData[lessonTitle];
-
-    modal.classList.add("active");
-  });
-});
-
-closeModal.addEventListener("click", () => {
-  modal.classList.remove("active");
-});
-
-window.addEventListener("click", e => {
-  if (e.target === modal) {
-    modal.classList.remove("active");
-  }
-});
 const seriesButtons = document.querySelectorAll(".video-lesson-card button");
 
 const seriesModal = document.querySelector("#seriesModal");
